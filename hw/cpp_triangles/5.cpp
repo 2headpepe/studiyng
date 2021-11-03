085a1de284341a66c7bd8b4caa5881ddc2a2c648
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-11-03 18:16:04 +0700

    Create 5.cpp

---
+++ hw/cpp_triangles/5.cpp
@@ -0,0 +1,27 @@
+//Задача 5. Вывести клетчатый ромб из зведочек. Высоту половины ромба задает пользователь.
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Enter Height/2\n ";
+    int num;
+    std::cin >> num;
+
+    for (int i = 1; i <= num; ++i)
+    {
+        for (int j = 0; j < num - i; j++) std::cout << " ";
+        for (int k = 0; k < i; k++) std::cout << "* ";
+        std::cout << std::endl;
+
+    }
+
+    for (int i=num-1; i >= 0; --i)
+    {
+        for (int j = 0; j < num - i; ++j) std::cout << " ";
+        for (int k = 0; k < i; ++k) std::cout << "* ";
+        std::cout << std::endl;
+
+    }
+    return 0;
+}
