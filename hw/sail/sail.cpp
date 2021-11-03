68d5aee9e2f4f6c1f152921eb84c2af32fd86779
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-11-03 22:13:46 +0700

    Create sail.cpp

---
+++ hw/sail/sail.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(){
+  float x,y;
+  std::cin>>x>>y;
+  bool b=false;
+  if (((x+1)*(x+1)+y*y<1)&&(y<=0)) b=true;
+  if (((x-1)*(x-1)+y*y<1)&&(y<=0)) b=true;
+  if ((y<=x/2+2)&&(x<=0)&&(x>=-1)&&(y>=0)) b=true;
+  if ((y<=x/2+2)&&(x<=0)&&(x>=-1)&&(y>=0)) b=true;
+  if ((y<=-2*x+2)&&(x<=1)&&(x>=0)&&(y>=0)) b=true;
+  
+  if(b) std::cout<<"in";  
+  if(!b) std::cout<<"out";
+}
