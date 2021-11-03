3c7bd874359804f8a7de8480c61c245e9ad3e6fb
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-11-03 18:14:04 +0700

    Create 3.cpp

---
+++ hw/cpp_triangles/3.cpp
@@ -0,0 +1,24 @@
+//Задача 3. Вывести ромб из зведочек. Высоту половины ромба задает пользователь.
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter Height/2\n ";
+    int num;
+    std::cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 0; j < num - i; j++) std::cout << " ";
+        for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
+        std::cout << std::endl;
+    }
+
+    for (int i=num-2; i >= 0; i--)
+    {
+        for (int j = 0; j < num - i; j++) std::cout << " ";
+        for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
+        std::cout << std::endl;
+    }
+    return 0;
+}
