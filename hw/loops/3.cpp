5edf8b19e25a50fc66b5bc90bb853504381cf4b9
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-11-03 20:25:42 +0700

    Create 3.cpp

---
+++ hw/loops/3.cpp
@@ -0,0 +1,15 @@
+//Для заданных значений n и x вычислить выражение: S = sin(x) + sin(sin(x)) + ... + sin(sin( ... (sin (x)) ... ))
+#include <iostream>
+#include<cmath>
+
+int main() {
+    int n;
+    float x;
+    std::cin>>n>>x;
+    float S=0.,curr=sin(x);
+    for(int i=1;i<n;++i){
+        S+=curr;
+        curr=sin(curr);
+    }
+    std::cout<<S;
+}
