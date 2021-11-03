e511e7d2afd9c201751734b8f06051b1592054cd
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-11-04 03:09:05 +0700

    Create 2.cpp

---
+++ hw/arrays/2.cpp
@@ -0,0 +1,20 @@
+//Схема Горнера.Коэффициенты многочлена находятся в массиве A размерности(N + 1), где N -- степень многочлена.Вычислить значение этого многочлена в точке x, то есть A[N]x^ N + ... + A[1]x + A[0], используя схему Горнера.
+#include <iostream>
+int main() {
+	int N;
+	float x;
+	std::cin >> N >> x;
+	int* m = new int[N+1];
+	int sum = 0;
+	float curr = 1;
+	for (int i = 0; i <= N; ++i)
+	{
+		std::cout << i << ": ";
+		std::cin >> m[i];
+		sum += m[i] * curr;
+		curr *= x;
+		std::cout<< "    : " << sum<<std::endl;
+	}
+	
+	delete [] m;
+}
