f0f8170ef1c529ef5a1e560f3b796fc2f7b7b69f
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-11-03 18:18:31 +0700

    Create 4.cpp

---
+++ hw/cpp_triangles/4.cpp
@@ -0,0 +1,38 @@
+//Задача 4. Вывести несколько ромбов из звездочек. Высоту половины ромба и количество ромбов задает пользователь.
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Enter Height/2\n";
+    int num;
+    std::cin >> num;
+    std::cout << "Enter quantity\n";
+    int quan;
+    std::cin >> quan;
+
+    for (int i = 1; i <= num; ++i)
+    {
+        for (int counter = 0; counter < quan + 1; ++counter)
+        {
+            for (int j = 0; j < num - i; j++) std::cout << " ";
+            for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
+            for (int j = 0; j < num - i; j++) std::cout << " ";
+        }
+        std::cout << std::endl;
+
+    }
+
+    for (int i=num-1; i >= 0; --i)
+    {
+        for (int counter = 0; counter < quan + 1; ++counter)
+        {
+            for (int j = 0; j < num - i; ++j) std::cout << " ";
+            for (int k = 0; k < i * 2 - 1; ++k) std::cout << "*";
+            for (int j = 0; j < num - i; ++j) std::cout << " ";
+        }
+        std::cout << std::endl;
+
+    }
+    return 0;
+}
