e30b8165b9dc0c8c2af3681500514546bc65cdcb
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-11-03 21:40:11 +0700

    Create 12.cpp

---
+++ hw/loops/12.cpp
@@ -0,0 +1,37 @@
+//Перевести заданное пользователем число (целое, положительнео) в 2-ю систему счисления и вывести на экран.
+#include<iostream>
+int main(){
+  int x,N;
+  std::cin>>x>>N;
+  int res=0,i=1;
+  if(N>10){
+    while(x){
+      i=1;
+      int newx=x;
+      while(newx>=16){
+        newx/=16;
+        i*=16;  
+      }
+      x-=i*newx;
+      switch(newx){
+        case 10: std::cout<<"A";break;
+        case 11: std::cout<<"B";break;
+        case 12: std::cout<<"C";break;
+        case 13: std::cout<<"D";break;
+        case 14: std::cout<<"E";break;
+        case 15: std::cout<<"F";break;
+        default:std::cout<<newx;break;
+      }
+    }
+  }
+  else if (N==10) std::cout<<x;
+  else
+  {
+    while(x){
+      res+=i*(x%N);
+      i*=10;
+      x/=N;
+    }
+    std::cout<<res;
+  }
+}
