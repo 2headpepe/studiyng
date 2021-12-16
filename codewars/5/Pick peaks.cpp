beffc83f5c0d67fe78a340ec66fe37509bd87265
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-12-16 22:58:43 +0300

    Create Pick peaks.cpp

---
+++ codewars/5/Pick peaks.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+PeakData pick_peaks(const std::vector<int> &v) {
+  PeakData result;
+  std::vector<int> x,peak;
+  for(int i=1;i<(int)v.size()-1;++i)
+  {
+    if((v[i]>v[i-1])&&(v[i]>v[i+1]))
+    {
+      x.push_back(i);
+      peak.push_back(v[i]);
+    }
+    if((v[i]>v[i-1])&&(v[i]==v[i+1]))
+    {
+      int j=i+1;
+      while((v[j]==v[j-1])&&(j<v.size()-1)) ++j;
+      if(v[j]<v[i]) 
+      {
+        x.push_back(i);
+        peak.push_back(v[i]);
+      }
+    }
+  }
+  result.pos=x;
+  result.peaks=peak;
+  return result;
+}
