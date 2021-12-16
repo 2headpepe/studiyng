ed6bb248a0f1df098685f80ff75795b6a128a54a
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-12-16 16:41:24 +0300

    Create 7.cpp

---
+++ hw/loops/7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+double smthrnd() {
+    return (double)rand() / RAND_MAX;
+}
+
+int main() {
+    int n;
+    std::cout << "Enter n:";
+    std::cin >> n;
+    double points_in = 0.;
+    for (int i = 0; i < n; i++)
+    {
+        double x = smthrnd();
+        douvle y = smthrnd();
+        points_in += (x * x + y * y) <= 1. ? 1. : 0.;
+    }
+    std::cout << (points_in / (double)n) << std::endl;
+}
