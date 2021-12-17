8e4bb73d293f2bfb01d80c0caef86dd65f8b958f
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-12-17 11:52:47 +0300

    Update 4.cpp

--- hw/arrays/4.cpp
+++ hw/arrays/4.cpp
@@ -4,14 +4,10 @@
 
 int main() {
     int n;
-    std::cout << "enter n: ";
     std::cin >> n;
     int* array = new int[n];
-    std::cout << "enter elements of array: ";
     for (int i = 0; i < n; ++i)
         std::cin >> array[i];
-    std::cout << "collapsing array: ";
-    bool check = n == (n / 2) * 2 ? true : false;
     if(n%2==0)
       for (int i = 0; i < n / 2; ++i)
         std::cout<<array[i]<<" "<<array[n-i-1]<<" ";
