e4372e710595bd3c3aa39cd245188abb1385c602
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-12-18 02:33:39 +0300

    Update 2.cpp

--- hw/bmp.md/2.cpp
+++ hw/bmp.md/2.cpp
@@ -1,31 +1,62 @@
 //поворотик
-void rotating(const char* in = "anime.bmp", int rotate = 90, const char* out = "Rotated_pic.bmp")//50/50
+void rotation(int rotate = 90,const char* inpic = "NOHOMO.bmp", const char* outpic = "Rotated_pic.bmp")//Sarmat<3
 {
-	RgbImg img = readRgbImg(in);
-	RgbImg new_img = createRgbImg(img.width, img.height, { 0,0,0 });
-	rotate %= 360;
-	//while (rotate)
-	//{
-		//rotate -= 90;
-		for (size_t i = 0; i < img.width; ++i)
+	RgbImg img = readRgbImg(inpic);
+
+	if (rotate % 90 != 0)
+	{
+		std::cout << "Wrong angle!\n";
+	}
+	else {
+		rotate %= 360;
+		if (rotate == 90)
 		{
-			for (size_t j = 0; j < img.height; ++j)
+			RgbImg new_img = createRgbImg(img.width, img.height, { 0, 0, 0 });
+			for (size_t x = 0; x < img.width; x++)
 			{
-				new_img.pixels[i][j].Blue = img.pixels[j][i].Blue;
-				new_img.pixels[i][j].Red = img.pixels[j][i].Red;
-				new_img.pixels[i][j].Green = img.pixels[j][i].Green;
+				for (size_t y = 0; y < img.height; y++)
+				{
+					new_img.pixels[img.width- 1 - x][y] = img.pixels[y][x];
+				}
 			}
+			writeRgbImg(outpic, new_img);
+			deleteRgbImg(img);
+			deleteRgbImg(new_img);
 		}
-	//}
-	writeRgbImg(out, new_img);
-	deleteRgbImg(new_img);
-	deleteRgbImg(img);
-}		
+		else if (rotate == 180) {
+            RgbImg new_img = createRgbImg(img.height, img.width, { 0, 0, 0 });
+            for (size_t x = 0; x < img.width; x++)
+            {
+                for (size_t y = 0; y < img.height; y++)
+                {
+                    new_img.pixels[y][x] = img.pixels[img.height- y - 1][img.width - x - 1];
+                }
+            }
+            writeRgbImg(outpic, new_img);
+            deleteRgbImg(img);
+            deleteRgbImg(new_img);
+        }
+        else if (rotate == 270) {
+            RgbImg new_img = createRgbImg(img.width, img.height, { 0, 0, 0 });
+            for (size_t x = 0; x < img.width; x++)
+            {
+                for (size_t y = 0; y < img.height; y++)
+                {
+                    new_img.pixels[x][img.height - 1 - y] = img.pixels[y][x];
+                }
+            }
+            writeRgbImg(outpic, new_img);
+            deleteRgbImg(new_img);
+            deleteRgbImg(img);
+        }
+    }
+}
 int main(int argc, const char* argv[])
 {		
 	try {
-		rotating();
-
+		//prep_for_conv();
+		//Mediana();
+		rotation();
 	}
 	catch (std::exception const& e)
 	{
