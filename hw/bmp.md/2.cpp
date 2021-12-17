5177184695f783983eb8d903f4df49983f556374
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-12-17 18:34:55 +0300

    Update 2.cpp

--- hw/bmp.md/2.cpp
+++ hw/bmp.md/2.cpp
@@ -1,22 +1,22 @@
-void rotating(const char* in = "bird.bmp", int rotate = 90, std::string out = "Rotated_pic.bmp")
+void rotating(const char* in = "anime.bmp", int rotate = 90, const char* out = "Rotated_pic.bmp")//50/50
 {
 	RgbImg img = readRgbImg(in);
 	RgbImg new_img = createRgbImg(img.width, img.height, { 0,0,0 });
 	rotate %= 360;
-	while (rotate)
-	{
-		rotate -= 90;
+	//while (rotate)
+	//{
+		//rotate -= 90;
 		for (size_t i = 0; i < img.width; ++i)
 		{
 			for (size_t j = 0; j < img.height; ++j)
 			{
-				new_img.pixels[j][i].Blue = img.pixels[i][j].Blue;
-				new_img.pixels[j][i].Red = img.pixels[i][j].Red;
-				new_img.pixels[j][i].Green = img.pixels[i][j].Green;
+				new_img.pixels[i][j].Blue = img.pixels[j][i].Blue;
+				new_img.pixels[i][j].Red = img.pixels[j][i].Red;
+				new_img.pixels[i][j].Green = img.pixels[j][i].Green;
 			}
 		}
-	}
-	writeRgbImg("SARMATlove.bmp", new_img);
+	//}
+	writeRgbImg(out, new_img);
 	deleteRgbImg(new_img);
 	deleteRgbImg(img);
 }		
