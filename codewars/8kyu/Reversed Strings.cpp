067e042f7133dbe4f4c7001ba18bd38fe31e8b91
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-12-17 11:43:31 +0300

    Update Reversed Strings.cpp

--- codewars/8kyu/Reversed Strings.cpp
+++ codewars/8kyu/Reversed Strings.cpp
@@ -3,7 +3,7 @@ using namespace std ;
 
 string reverseString (string str )
 {
-  string new_str="";
-  for(int i=str.length()-1;i>=0;--i) new_str+=str[i];
+  string new_str=str;
+  for(int i=str.length()-1;i>=0;--i) new_str[i]=str[i];
   return new_str ;
 }
