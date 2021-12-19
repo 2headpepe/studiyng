5f0d499285cbb4d80d2bfc8a30fd0690cde92d20
2headpepe <93649485+2headpepe@users.noreply.github.com>
2021-12-19 18:58:54 +0300

    Update lessgonavi.cpp

--- lessgonavi.cpp
+++ lessgonavi.cpp
@@ -1,6 +1,61 @@
 #include<fstream>
 #include <iostream>
+/*
+void print(double** matrix, int rows,int cols)
+{
+  for(int i=0;i<rows;++i)
+  {
+    for(int j=0;j<cols;++j)
+    {
+      std::cout.width(4);
+      std::cout<<matrix[i][j];
+      
+    }
+    std::cout<<std::endl;
+    
+  }
+  for (int i = 0; i < rows; ++i) delete[] matrix[i];
+  delete[] matrix;
+}
+double** create(size_t rows,size_t cols)
+{
+  double** matrix = new double* [rows];
+  for (int i = 0; i < rows; ++i) matrix[i] = new double[cols];
+  return matrix;
+}
+double** inputfile(const char* name, size_t rows,size_t cols)
+{
+  double** matrix =create(rows,cols);
+  std::ifstream in(name);
+  if (!in.is_open())
+      throw std::runtime_error("Failed to open matrix file");
+  for (int r = 0; r < rows; ++r)
+    for (int c = 0; c < cols; ++c)
+      in >> matrix[r][c]; //*(*(matrix+r)+c)
+  
+  in.close();
+  
+  for (int i = 0; i < rows; ++i) delete[] matrix[i];
+  delete[] matrix;
 
+
+  return matrix;
+      
+}
+int main()
+{
+  
+        int rows = 6, cols = 6;
+        double** matrix = inputfile("matrix.txt",rows, cols);
+        
+        print(matrix,rows,cols);
+        
+      
+        
+        for (int i = 0; i < rows; ++i) delete[] matrix[i];
+        delete[] matrix;
+   
+}*/
 double** createMatrix(size_t rows, size_t cols)
 {
     double** matrix = new double* [rows];
@@ -75,7 +130,19 @@ void print_file(double** matrix,size_t rows,size_t cols)
     navi<<std::endl;
   }   
 }
+double** rotate180(double** matrix,size_t rows,size_t cols)
+{
+  double** new_matrix=createMatrix(rows,cols);
+  for(int i =0;i<rows;++i)
+  {
+    for(int j=0;j<cols;++j)
+    {
+      new_matrix[rows-i-1][cols-j-1]=matrix[i][j];
+    }
+  }
+  return new_matrix;
+}
 int main()
 {
-  print_file(snake(6,10),6,10);
+  print_file(rotate180(snake(6,10),6,10),6,10);
 }
