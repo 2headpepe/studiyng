b60daecad86418c3b7cd2382e17c5e554f3e794d
2headpepe <93649485+2headpepe@users.noreply.github.com>
2022-02-26 13:34:18 +0300

    Update database.cpp

--- hw 2sem/1project/database.cpp
+++ hw 2sem/1project/database.cpp
@@ -1,8 +1,8 @@
 #include "database.h"
-
+#include<fstream>
 std::ostream& operator<<(std::ostream& out, const Flat& num)
 {
-	out << num.id << ", " << num.price << ", " << num.countRooms << ", " << num.floor << ", " << num.area << ", ";
+	out << num.id <<'\t'  << num.price << '\t' << num.countRooms << '\t'<<'\t' << num.floor << '\t' << num.area << '\t';
 	int i = 0;
 	while(num.address[i]!='\a')
 	{
@@ -25,7 +25,8 @@ std::istream& operator>>(std::istream& in, Flat& num)
 	std::cout << "Area: ";
 	in >> num.area;
 	std::cout << "Address: ";
-	in>>num.address;
+	in.ignore();
+	in.getline(num.address,64);
 	
 	//num.id = db.count;
 	return in;
@@ -51,8 +52,73 @@ int addFlat(DataBase& db)
 }
 void printDB(const DataBase& db)
 {
+	std::cout << "ID" << '\t' << "Price" << '\t' << "CountRooms" << '\t' << "Floor" << '\t' << "Area" << '\t' << "Address" << std::endl;
 	for (int i = 0; i < db.count; ++i)
 	{
 		std::cout << db.data[i] << std::endl;
 	}
 }
+void exportDB(const DataBase& db, const char* filename)
+{
+	std::ofstream outFile(filename,std::ios_base::binary);
+	if (!outFile.is_open())
+	{
+		std::cout << "Can't open :(";
+		return;
+	}
+
+	outFile << "ID" << '\t' << "Price" << '\t' << "CountRooms" << '\t' << "Floor" << '\t' << "Area"<<'\t' << "Address" << std::endl;
+	for (int i = 0; i < db.count; ++i)
+	{
+		outFile << db.data[i];
+		outFile << std::endl;
+	}
+
+	outFile.close();
+
+}
+void saveDB(const DataBase& db, const char* filename)
+{
+	std::ofstream outFile(filename, std::ios_base::binary);
+	if (!outFile.is_open())
+	{
+		std::cout << "Can't open :(";
+		return;
+	}
+	outFile.write((const char*)&db.count, sizeof(int));
+	outFile.write((const char*)db.data, db.count * sizeof(Flat));
+
+	outFile.close();
+}
+void openDB(DataBase& db, const char* filename)
+{
+	std::ifstream in(filename,std::ios_base::binary);
+	if (!in.is_open())
+	{
+		std::cout << "Can't open :(";
+		return;
+	}
+
+	int count;
+	in.read((char*)&count, sizeof(int));
+	
+	if (db.data) delete[] db.data;
+	db.data = new Flat[count];
+	db.count = count;
+	in.read((char*)db.data, sizeof(Flat) * db.count);
+	in.close();
+	
+}
+//void delDB(DataBase& db)
+//{
+//	int tmp;
+//	std::cout << "ID to delete: ";
+//	std::cin >> tmp;
+//
+//
+//	for (int i = 0; i < db.count; ++i)
+//		if (db.data[i].id == tmp) delete	 db.data[i]
+//}
+
+
+
