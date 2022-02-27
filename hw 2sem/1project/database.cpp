87e92a33412f67bc5fa2b95dbcf13620acd0deca
2headpepe <93649485+2headpepe@users.noreply.github.com>
2022-02-27 15:29:10 +0300

    Update database.cpp

--- hw 2sem/1project/database.cpp
+++ hw 2sem/1project/database.cpp
@@ -1,42 +1,7 @@
 #include "database.h"
 #include<fstream>
 #include<iomanip>
-std::ostream& operator<<(std::ostream& out, const Flat& num)
-{
-	out << num.id << '\t' << std::setfill('0') << std::setw(2) << num.day << '.' << std::setfill('0')<< std::setw(2)
-		<< num.month << '.' << std::setfill('0')<< std::setw(2) << num.year << '\t' << num.price << '\t' << num.countRooms
-															<< '\t' << '\t' << num.floor << '\t' << num.area << '\t';
-	int i = 0;
-	while(num.address[i]!='\a')
-	{
-		out << num.address[i];
-		++i;
-	}
-	out << " ";
-	return out;
-}
-std::istream& operator>>(std::istream& in, Flat& num)
-{
-	for (int i = 0; i < 64; ++i) num.address[i] = '\a';
-	std::cout << "Date. Year: "; in >> num.year;
-	std::cout << "Date. No. of month: "; in >> num.month;
-	std::cout << "Date. Day: "; in >> num.day;
-	std::cout << "Price: ";
-	in >> num.price;
-	in.ignore();
-	std::cout << "Rooms: ";
-	in >> num.countRooms;
-	std::cout << "Floor: ";
-	in >> num.floor;
-	std::cout << "Area: ";
-	in >> num.area;
-	std::cout << "Address: ";
-	in.ignore();
-	in.getline(num.address,64);
-	
-	//num.id = db.count;
-	return in;
-}
+
 int addFlat(DataBase& db)
 {
 	Flat* tmp = new Flat[db.count + 1];
@@ -122,16 +87,28 @@ void openDB(DataBase& db, const char* filename)
 	in.close();
 	
 }
-//void delDB(DataBase& db)
-//{
-//	int tmp;
-//	std::cout << "ID to delete: ";
-//	std::cin >> tmp;
-//
-//
-//	for (int i = 0; i < db.count; ++i)
-//		if (db.data[i].id == tmp) delete	 db.data[i]
-//}
+void delDB(DataBase& db)
+{
+	int tmp;
+	std::cout << "ID to delete: ";
+	std::cin >> tmp;
+	if (tmp < 0) return;
+
+	for (int i = tmp + 1; i < db.count; ++i)
+		db.data[i - 1] = db.data[i];
+	--db.count;
+}
+void sortDB(DataBase& db)
+{
+	std::cout << "Count rooms: ";
+	int count;
+	std::cin >> count;
+	
+	mySort(db.data,db.count, sortByCountRooms,count);
+	printDB(db);
+}
+
+
 
 
 
