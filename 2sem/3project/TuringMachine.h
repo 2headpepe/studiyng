a834d26f7dda4d51aa20cc497231364869b45e7c
2headpepe <93649485+2headpepe@users.noreply.github.com>
2022-05-04 20:42:42 +0300

    Create TuringMachine.h

---
+++ 2sem/3project/TuringMachine.h
@@ -0,0 +1,75 @@
+#pragma once
+#include<string>
+#include<iostream>
+#include<fstream>
+#include<vector>
+
+class rule
+{
+public:
+    char in;     // Входной символ '\0' - последний
+    char* cond;     // Состояние. 'S' - начальное. 'Q' - завершение работы
+
+    char out;    // Выходной символ
+    char* newCond;  // Новое состояние
+    char dir;
+
+    int shift; //оптимизация перехода в новое состояние
+
+};
+
+
+char* endingCond(rule* (&rls), const int& len);
+std::string strToNormal(const std::string(&str), const int len);
+void shifting(rule* (&rls), const int(&count));
+
+class TuringMachine
+{
+    rule* rules;
+    int countRules = 0;
+    int maxLen;
+    char* endCond;
+public:
+
+
+    TuringMachine()
+    {
+        countRules = 0;
+        rules = new rule[0];
+        maxLen = 0;
+        //shifting(rules, countRules);
+        //endCond = endingCond(rules, countRules);
+    }
+    TuringMachine(int len, rule* rls, int c)
+    {
+        countRules = c;
+        rules = new rule[c];
+        maxLen = len;
+        for (int i = 0; i < c; ++i) rules[i] = rls[i];
+        shifting(rules, countRules);
+        endCond = endingCond(rules, countRules);
+    }
+    TuringMachine(TuringMachine const& mac)
+    {
+        countRules = mac.countRules;
+        rules = new rule[countRules];
+        for (int i = 0; i < countRules; ++i) rules[i] = mac.rules[i];
+        maxLen = mac.maxLen;
+        shifting(rules, countRules);
+        endCond = endingCond(rules, countRules);
+    }
+    TuringMachine(TuringMachine&& mac): countRules(std::move(mac.countRules)), rules(std::move(mac.rules)), maxLen(std::move(mac.maxLen))
+    {
+        mac.rules = nullptr;
+
+        shifting(rules, countRules);
+        endCond = endingCond(rules, countRules);
+    }
+
+    std::string calc(std::string &init, int maxsteps);
+    TuringMachine begining(std::ifstream (&prog), std::ifstream (&input), std::ofstream (&out));
+};
+
+
+
+
